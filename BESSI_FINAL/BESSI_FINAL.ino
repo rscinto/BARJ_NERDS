861483f233a233f7dbe2eee32604078537472a71
Benjamin T <macwebdzyne@gmail.com>
2022-09-08 15:50:04 -0700

    Moving some code around

--- BESSI_FINAL/BESSI_FINAL.ino
+++ BESSI_FINAL/BESSI_FINAL.ino
@@ -1,9 +1,6 @@
 #include <IRremote.h>
 
 
-
-
-
 /*-----------------------HEADLIGHTS-------------------*/
 
 // LED definitions
@@ -31,119 +28,32 @@ int maxDarkValue;
 int darknessValue;
 int waterValue;
 
-//Unsigned so the lightLevel will never be negative
+//Circular array for brightness checking
+unsigned int lightValues[120] = {0};
+unsigned int lightValuesIndex = 0;
+
+//Calculated average brightness
+float lightValuesAvg = 0;
+float lightValuesLowAvg = 0;
+
+//Unsigned input light sensor values
+unsigned int currentLightValue;
+unsigned int prevLightValue;
+
+//Target and current LED brightness levels
+unsigned int targetLEDBrightnessLevel = 0;
+unsigned int currentLEDBrightnessLevel = 0;
+
+//Unsigned because input should always be 
 unsigned int lightLevel;
-//This is called when CEILING_BUTTON (3) is pressed.
-//The 
-void setCeiling() {
-  maxDarkValue = analogRead(LIGHT_SENSOR);
-  if (maxDarkValue > MAX_DARKNESS_VALUE) {
-    maxDarkValue = MAX_DARKNESS_VALUE;
-  }
-  
-  for(int i = 0; i < 3; i++) {
-    blink();
-  }
-  
-  Serial.println("CEILING BUTTON PRESSED");
-}
-
-void setFloor() {
-  minDarkValue = analogRead(LIGHT_SENSOR);
-  if (minDarkValue < MIN_DARKNESS_VALUE) {
-    minDarkValue = MIN_DARKNESS_VALUE;
-  }
-  
-  
-  for(int i = 0; i < 3; i++) {
-    blink();
-  }
-  Serial.println("FLOOR BUTTON PRESSED");
-}
 
 
-void blink() {
-  digitalWrite(LED_1, LOW);
-  digitalWrite(LED_2, LOW);
-  delay(750);
-  digitalWrite(LED_1, HIGH);
-  digitalWrite(LED_2, HIGH);
-  delay(750);
-  digitalWrite(LED_1, LOW);
-  digitalWrite(LED_2, LOW);
-  delay(750); 
-  digitalWrite(LED_1, HIGH);
-  digitalWrite(LED_2, HIGH);
-  delay(750);
-  Serial.println("BLINKING");
-} 
-
-
-void setupLights() {
- pinMode(LED_1, OUTPUT);
- pinMode(LED_2, OUTPUT);
-
- pinMode(WATER_SENSOR, INPUT);
-
- pinMode(FLOOR_BUTTON, INPUT_PULLUP);
- pinMode(CEILING_BUTTON, INPUT_PULLUP);
-
- digitalWrite(LED_1, LOW);
- digitalWrite(LED_2, LOW);
-
- //Listen for button presses
- attachInterrupt(digitalPinToInterrupt(FLOOR_BUTTON), setFloor, RISING);
- attachInterrupt(digitalPinToInterrupt(CEILING_BUTTON), setCeiling, RISING);
-
- // Setup default variable values
- minDarkValue = MIN_DARKNESS_VALUE;
- maxDarkValue = MAX_DARKNESS_VALUE;
-}
-
-
-void headlights() {
-  darknessValue = analogRead(LIGHT_SENSOR); 
-  waterValue = analogRead(WATER_SENSOR);
-
-  if (darknessValue< MIN_DARKNESS_VALUE) {
-    darknessValue = MIN_DARKNESS_VALUE;
-  }
-
-  if (darknessValue > MAX_DARKNESS_VALUE) {
-    darknessValue = MAX_DARKNESS_VALUE;
-  }
-
-  //For testing purpose
-  Serial.println("Analog Darkness value: " + String(darknessValue));
-  Serial.println("Light Level: " + String(lightLevel));
-  Serial.println("Analog Water value: " + String(waterValue));
-  Serial.println("minDarkValue: " + String(minDarkValue));
-  Serial.println("maxDarkValue: " + String(maxDarkValue));
-
-  // Map measured environmental light values to RGB LED's lowest to highest brightness settings
-  lightLevel = map(darknessValue, minDarkValue, maxDarkValue, 0, 255);
-
-  delay(100); // use delay for user perceived smoothness
-
-  if (waterValue > WATER_THRESHOLD)
-  {
-    Serial.print("\nWater detected!\n");
-    lightLevel = 255;
-    delay(50);
-    waterValue = analogRead(WATER_SENSOR);
-  }
-
-  analogWrite(LED_1, lightLevel);
-  analogWrite(LED_2, lightLevel);
-
-  delay(100);
-}
-
-/*-----------------------END HEADLIGHTS---------------*/
-
-
-
-
+//Function prototypes
+void setCeiling();
+void setFloor();
+void blink();
+void setupLights();
+void headlights();
 
 
 //--------------ULTRA SONIC-----------------------
@@ -276,7 +186,7 @@ void setup()
 
 
 
-
+// BEGIN MAIN LOOP
 void loop()
 {
   headlights();
@@ -314,4 +224,118 @@ void loop()
       irrecv.resume(); 
   }
   */
+}
+// END LOOP
+
+
+
+// Full function definitions
+
+//This is called when CEILING_BUTTON (3) is pressed.
+void setCeiling() {
+  Serial.println("CEILING BUTTON PRESSED");
+  
+  maxDarkValue = analogRead(LIGHT_SENSOR);
+
+  // above a certain value, just consider us at maximum brightness
+  if (maxDarkValue > MAX_DARKNESS_VALUE) {
+    maxDarkValue = MAX_DARKNESS_VALUE;
+  }
+  
+  blink();
+}
+
+//This is called when FLOOR_BUTTON (2) is pressed.
+void setFloor() {
+  
+  minDarkValue = analogRead(LIGHT_SENSOR);
+  
+  // below a certain value, just consider us at minimum darkness
+  if (minDarkValue < MIN_DARKNESS_VALUE) {
+    minDarkValue = MIN_DARKNESS_VALUE;
+  }
+  
+  
+  for(int i = 0; i < 3; i++) {
+    blink();
+  }
+  Serial.println("FLOOR BUTTON PRESSED");
+}
+
+
+void blink() {
+  Serial.println("BLINKING");
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, LOW);
+  delay(250);
+  digitalWrite(LED_1, HIGH);
+  digitalWrite(LED_2, HIGH);
+  delay(250);
+  digitalWrite(LED_1, LOW);
+  digitalWrite(LED_2, LOW);
+  delay(250); 
+  digitalWrite(LED_1, HIGH);
+  digitalWrite(LED_2, HIGH);
+  delay(250);
+} 
+
+
+void setupLights() {
+ pinMode(LED_1, OUTPUT);
+ pinMode(LED_2, OUTPUT);
+
+ pinMode(WATER_SENSOR, INPUT);
+
+ pinMode(FLOOR_BUTTON, INPUT_PULLUP);
+ pinMode(CEILING_BUTTON, INPUT_PULLUP);
+
+ digitalWrite(LED_1, LOW);
+ digitalWrite(LED_2, LOW);
+
+ //Listen for button presses
+ attachInterrupt(digitalPinToInterrupt(FLOOR_BUTTON), setFloor, RISING);
+ attachInterrupt(digitalPinToInterrupt(CEILING_BUTTON), setCeiling, RISING);
+
+ // Setup default variable values
+ minDarkValue = MIN_DARKNESS_VALUE;
+ maxDarkValue = MAX_DARKNESS_VALUE;
+}
+
+
+void headlights() {
+  darknessValue = analogRead(LIGHT_SENSOR); 
+  waterValue = analogRead(WATER_SENSOR);
+
+  if (darknessValue< MIN_DARKNESS_VALUE) {
+    darknessValue = MIN_DARKNESS_VALUE;
+  }
+
+  if (darknessValue > MAX_DARKNESS_VALUE) {
+    darknessValue = MAX_DARKNESS_VALUE;
+  }
+
+  //For testing purpose
+  Serial.println("Analog Darkness value: " + String(darknessValue));
+  Serial.println("Light Level: " + String(lightLevel));
+  Serial.println("Analog Water value: " + String(waterValue));
+  Serial.println("minDarkValue: " + String(minDarkValue));
+  Serial.println("maxDarkValue: " + String(maxDarkValue));
+
+  // Map measured environmental light values to RGB LED's lowest to highest brightness settings
+  lightLevel = map(darknessValue, minDarkValue, maxDarkValue, 0, 255);
+
+  delay(100); // use delay for user perceived smoothness
+
+  if (waterValue > WATER_THRESHOLD)
+  {
+    Serial.print("\nWater detected!\n");
+    lightLevel = 255;
+    delay(50);
+    waterValue = analogRead(WATER_SENSOR);
+  }
+
+  analogWrite(LED_1, lightLevel);
+  analogWrite(LED_2, lightLevel);
+
+  delay(100);
 }
